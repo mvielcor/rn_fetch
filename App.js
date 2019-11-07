9fa127e9519769bd5f89881e757dbca23e87814a
Manel <mviel@florida-uni.es>
2019-11-07 10:09:36 +0100

    Exercici1

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      documentJSON: [],
+    };
+  }
+  //https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then((resposta) => {
+        if (resposta.ok) {
+          return resposta.json();
+        } else {
+          console.log("Error connectant amb https://jsonplaceholder.typicode.com/posts/")
+        }
+      })
+      .then(respostaJson => {
+        console.log(respostaJson);
+        this.setState({ documentJSON: respostaJson })
+      })
+      .catch(error => {
+        console.log("Error de xarxa: " + error);
+      });
+  }
+
+  render() {
+    let msg = "Carregant ...";
+    return (
+      <View>
+        <Text style={styles.sectionTitle}>
+        {
+          (this.state.documentJSON != undefined ?
+            this.state.documentJSON.title : msg)
+        }
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
