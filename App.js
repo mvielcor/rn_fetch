e19798b14827622be5f991af74e7601de4300b23
Manel <mviel@florida-uni.es>
2019-11-07 15:35:50 +0100

    Exercici2

--- App.js
+++ App.js
@@ -9,11 +9,32 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  ScrollView,
   View,
   Text,
 } from 'react-native';
 
+export class ComponentsDeText extends Component{
+  render(){
+    let array = this.props.llistaComponents.map((unElement, pos) => {
+      if(pos%2===0){
+      return (<Text style={{fontWeight: 'bold'}}>{unElement.title}</Text>);
+      }else{
+        return (<Text>{unElement.title}</Text>);
+      }
+    });
 
+    return (
+      <View>
+        <ScrollView>
+          <View>
+           {array}
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
+}
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -21,9 +42,10 @@ export default class App extends Component {
       documentJSON: [],
     };
   }
+ 
   //https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts/')
+  UNSAFE_componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then((resposta) => {
         if (resposta.ok) {
           return resposta.json();
@@ -31,27 +53,24 @@ export default class App extends Component {
           console.log("Error connectant amb https://jsonplaceholder.typicode.com/posts/")
         }
       })
-      .then(respostaJson => {
-        console.log(respostaJson);
-        this.setState({ documentJSON: respostaJson })
-      })
+      .then(respostaJson => this.setState({ documentJSON: respostaJson }))
       .catch(error => {
         console.log("Error de xarxa: " + error);
       });
   }
-
+  
   render() {
-    let msg = "Carregant ...";
-    return (
-      <View>
-        <Text style={styles.sectionTitle}>
-        {
-          (this.state.documentJSON != undefined ?
-            this.state.documentJSON.title : msg)
-        }
-        </Text>
-      </View>
-    );
+    if (this.state.documentJSON.length != 0) {
+      return (
+        <View style={{flex:1}} >
+            <ComponentsDeText llistaComponents={this.state.documentJSON}></ComponentsDeText>
+        </View>
+      );
+    }else{
+      return (
+        <View><Text style={{fontSize:35}}>Carregant ...</Text></View>
+      );
+    }
   }
 }
 
