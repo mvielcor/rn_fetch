7017f3072179b0ad5d944ba84abb81a6485e4fa1
Manel <mviel@florida-uni.es>
2019-11-12 18:46:09 +0100

    commit

--- App.js
+++ App.js
@@ -11,15 +11,17 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  Button,
   Text,
 } from 'react-native';
 
-export class ComponentsDeText extends Component{
-  render(){
+
+export class ComponentsDeText extends Component {
+  render() {
     let array = this.props.llistaComponents.map((unElement, pos) => {
-      if(pos%2===0){
-      return (<Text style={{fontWeight: 'bold'}}>{unElement.title}</Text>);
-      }else{
+      if (pos % 2 === 0) {
+        return (<Text style={{ fontWeight: 'bold' }}>{unElement.title}</Text>);
+      } else {
         return (<Text>{unElement.title}</Text>);
       }
     });
@@ -28,7 +30,7 @@ export class ComponentsDeText extends Component{
       <View>
         <ScrollView>
           <View>
-           {array}
+            {array}
           </View>
         </ScrollView>
       </View>
@@ -42,10 +44,12 @@ export default class App extends Component {
       documentJSON: [],
     };
   }
- 
-  //https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
-  UNSAFE_componentWillMount() {
+
+  //Mètode per a connectar amb l'api i obtindre un llistat de tots els posts disponibles
+  obtinPosts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
+      //fetch('http://my-json-server.typicode.com/mvielcor/JSONServer/posts')
+      //fetch('https://localhost:3000/posts')
       .then((resposta) => {
         if (resposta.ok) {
           return resposta.json();
@@ -58,21 +62,113 @@ export default class App extends Component {
         console.log("Error de xarxa: " + error);
       });
   }
-  
+
+  insereixPost() {
+    let dades = {
+      title: 'PMDM',
+      id: 10
+    };
+    
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(dades),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then((resposta) => {
+        if (resposta.ok) {
+          return resposta.json();
+        } else {
+          console.log("Error fent el POST")
+        }
+      })
+      .then(respostaJson => {
+        console.log(respostaJson);
+        alert("Dades introduides correctament {" + dades.id + "," + dades.title + "}");
+      })
+      .catch(error => {
+        console.log("Error de xarxa: " + error);
+      });
+  }
+
+  deletePost() {
+    let url = 'https://jsonplaceholder.typicode.com/posts/1';
+    fetch(url, {
+      method: 'DELETE',
+    })
+      .then((resposta) => {
+        if (resposta.ok) {
+          return resposta.json();
+        } else {
+          console.log("Error " + resposta.status + " fent el DELETE")
+          return;
+        }
+      })
+      .then(respostaJson => {
+        console.log(respostaJson);
+        alert("Dades esborrades correctament");
+      })
+      .catch(error => {
+        console.log("Error de xarxa: " + error);
+      });
+  }
+
+  putPost() {
+    let dades = {
+      title: 'Manel'
+    };
+    let url = 'https://jsonplaceholder.typicode.com/posts/1';
+    fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(dades),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then((resposta) => {
+        if (resposta.ok) {
+          return resposta.json();
+        } else {
+          console.log("Error fent el PUT")
+        }
+      })
+      .then(respostaJson => {
+        console.log(respostaJson);
+        alert("Dades actualitzades correctament {" + dades.id + "," + dades.title + "}");
+      })
+      .catch(error => {
+        console.log("Error de xarxa: " + error);
+      });
+  }
+  //https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
+  UNSAFE_componentWillMount() {
+    this.obtinPosts();
+  }
+
   render() {
     if (this.state.documentJSON.length != 0) {
       return (
-        <View style={{flex:1}} >
+        <View style={{ flex: 1 }}>
+          <View style={{ flex: 0.75 }} >
             <ComponentsDeText llistaComponents={this.state.documentJSON}></ComponentsDeText>
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', paddingHorizontal: 15 }}>
+            <Button title='Insereix' onPress={this.insereixPost} ></Button>
+            <Button title='Delete' onPress={this.deletePost}></Button>
+            <Button title='Update' onPress={this.putPost}></Button>
+          </View>
         </View>
       );
-    }else{
+    } else {
       return (
-        <View><Text style={{fontSize:35}}>Carregant ...</Text></View>
+        <View><Text style={{ fontSize: 35 }}>Carregant ...</Text></View>
       );
     }
   }
 }
+
 
 const styles = StyleSheet.create({
   sectionContainer: {
